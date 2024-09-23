fffe80195b903fa02234b78630be194bfd30bbb0
James Onashile <jamesonashile@Jamess-MacBook-Pro.local>
2024-09-23 13:03:36 +0100

    resident

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <iframe
+        src="https://drive.google.com/file/d/1y0MhCDE0--obnwLjvWXoV9jO8VXr5xEm/preview"
+        id="portfolio"
+      ></iframe>
     </div>
   );
 }
